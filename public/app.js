011a3bcc0ee92e8ac268037cbd67b9443dd14574
Ahmed Raza Qadri <javateched@gmail.com>
2016-11-03 00:22:48 +0500

    Staric_Diamond_-_Table_Creator_-_Fibonacci_Num_Series_With_Javascript Assignment with a short profile protofoilo

---
+++ public/app.js
@@ -0,0 +1,69 @@
+// var num = "";
+// var size = prompt("Enter the size of the pyramid");
+// for(var i=1; i<=size; i++)
+// {
+//   num = num + i
+  
+//   document.write(num);
+//   document.write("<br>");
+
+// }
+// var x,y;
+//     for(x=0;x<5;x++)
+// {
+//     for(y=0;y<=x;y=y+1)
+// {
+//     document.write("*");
+// }
+//     document.write("<br>");
+// }
+
+// var a ,b;
+// var size = prompt("Enter the size of the pyramid");
+
+//     for(a = 1;a<=size;a++){
+//         for(b=1;b<=a;b++){
+//             document.write(" * ");
+//         }
+//         document.write("<br>");
+//     }
+// function diamond( n ) 
+// {
+//     n = parseInt(n,20);
+
+//     var i, s;
+
+//     // top: 1 to n
+//     document.write("<pre>");
+//     for(i = 1; i <= n; i++ )
+//     {
+//         // write n-i spaces:
+//         for ( s = 1; s <= n-i; s++ )
+//         {
+//             document.write(" ");
+//         }
+//         // then write i asterisk+space sets:
+//         for ( s = 1; s <= i; s++ )
+//         {
+//             document.write(" *");
+//         }
+//         document.write("\n");
+//     }
+//     // bottom: n-1 down to 1
+//     for(i = n-1; i >= 1; i-- )
+//     {
+//         // write n-i spaces:
+//         for ( s = 1; s <= n-i; s++ )
+//         {
+//             document.write(" ");
+//         }
+//         // then write i asterisk+space sets:
+//         for ( s = 1; s <= i; s++ )
+//         {
+//             document.write(" *");
+//         }
+//         document.write("\n");
+//     }
+// }
+
+// diamond(20);
